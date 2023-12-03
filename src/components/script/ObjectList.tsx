6d7a158d541b9722a612b7f369445562fc22a9e5
krystiandzirba <krystiandzirba8820@gmail.com>
2023-12-03 23:58:50 +0100

    0.7.0 changed ObjectList structure

--- src/components/script/ObjectList.tsx
+++ src/components/script/ObjectList.tsx
@@ -4,29 +4,29 @@ interface ObjectListProps {
 
 const ObjectList: React.FC<ObjectListProps> = ({ type }) => {
   const object_list = [
-    ["twig_foundation_low", "thumbnail", "FL0"],
-    ["wooden_foundation_low", "thumbnail", "FL1"],
-    ["stone_foundation_low", "thumbnail", "FL2"],
-    ["metal_foundation_low", "thumbnail", "FL3"],
-    ["armored_foundation_low", "thumbnail", "FL4"],
+    { name: "twig_foundation_low", thumbnail: "", id: "FL0" },
+    { name: "wooden_foundation_low", thumbnail: "", id: "FL1" },
+    { name: "stone_foundation_low", thumbnail: "", id: "FL2" },
+    { name: "metal_foundation_low", thumbnail: "", id: "FL3" },
+    { name: "armored_foundation_low", thumbnail: "", id: "FL4" },
 
-    ["twig_foundation_mid", "thumbnail", "FM0"],
-    ["wooden_foundation_mid", "thumbnail", "FM1"],
-    ["stone_foundation_mid", "thumbnail", "FM2"],
-    ["metal_foundation_mid", "thumbnail", "FM3"],
-    ["armored_foundation_mid", "thumbnail", "FM4"],
+    { name: "twig_foundation_mid", thumbnail: "", id: "FM0" },
+    { name: "wooden_foundation_mid", thumbnail: "", id: "FM1" },
+    { name: "stone_foundation_mid", thumbnail: "", id: "FM2" },
+    { name: "metal_foundation_mid", thumbnail: "", id: "FM3" },
+    { name: "armored_foundation_mid", thumbnail: "", id: "FM4" },
 
-    ["twig_foundation_high", "thumbnail", "FH0"],
-    ["wooden_foundation_high", "thumbnail", "FH1"],
-    ["stone_foundation_high", "thumbnail", "FH2"],
-    ["metal_foundation_high", "thumbnail", "FH3"],
-    ["armored_foundation_high", "thumbnail", "FH4"],
+    { name: "twig_foundation_high", thumbnail: "", id: "FH0" },
+    { name: "wooden_foundation_high", thumbnail: "", id: "FH1" },
+    { name: "stone_foundation_high", thumbnail: "", id: "FH2" },
+    { name: "metal_foundation_high", thumbnail: "", id: "FH3" },
+    { name: "armored_foundation_high", thumbnail: "", id: "FH4" },
 
-    ["twig_wall", "thumbnail", "W0"],
-    ["wooden_wall", "thumbnail", "W1"],
-    ["stone_wall", "thumbnail", "W2"],
-    ["metal_wall", "thumbnail", "W3"],
-    ["armored_wall", "thumbnail", "W4"],
+    { name: "twig_wall", thumbnail: "", id: "W0" },
+    { name: "wooden_wall", thumbnail: "", id: "W1" },
+    { name: "stone_wall", thumbnail: "", id: "W2" },
+    { name: "metal_wall", thumbnail: "", id: "W3" },
+    { name: "armored_wall", thumbnail: "", id: "W4" },
   ];
 
   return (
@@ -37,8 +37,8 @@ const ObjectList: React.FC<ObjectListProps> = ({ type }) => {
     >
       <div className="object_list">
         {object_list.map((item) => (
-          <div className="object" key={item[2]}>
-            {item[0]}
+          <div key={item.id} className="object">
+            {item.name}
           </div>
         ))}
       </div>
--- src/components/script/Version.tsx
+++ src/components/script/Version.tsx
@@ -1,3 +1,3 @@
 export default function Version() {
-  return <div className="version">RBB v.0.6.0</div>;
+  return <div className="version">RBB v.0.7.0</div>;
 }
