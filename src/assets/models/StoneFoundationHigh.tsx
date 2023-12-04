38ed9921e8bce34de1d205314e57d815657540f3
krystiandzirba <krystiandzirba8820@gmail.com>
2023-12-04 23:20:33 +0100

    v 0.8.0 / changed file structure / fixed model not loading correctly

--- src/assets/models/StoneFoundationHigh.tsx
+++ src/assets/models/StoneFoundationHigh.tsx
@@ -2,7 +2,10 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { useLoader } from "@react-three/fiber";
 
 const StoneFoundationHigh: React.FC = () => {
-  const gltf = useLoader(GLTFLoader, "./src/assets/models/stone_foundation_high.glb");
+  //  const gltf = useLoader(GLTFLoader, "/assets/models/stone_foundation_high.glb");
+
+  const gltf = useLoader(GLTFLoader, "./stone_foundation_high.glb");
+
   return <primitive object={gltf.scene} scale={1} />;
 };
 
--- src/components/script/Version.tsx
+++ src/components/script/Version.tsx
@@ -1,3 +1,3 @@
 export default function Version() {
-  return <div className="version">RBB v.0.7.0</div>;
+  return <div className="version">RBB v.0.8.0</div>;
 }
