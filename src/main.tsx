1e6052654c08edd840ff0f7f41af6bb24bc62c29
krystiandzirba <krystiandzirba8820@gmail.com>
2023-12-07 17:46:57 +0100

    0.19.0 / Added redux

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+import store from "./Store.tsx";
+import { Provider } from "react-redux";
+
+console.log(store.getState());
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
