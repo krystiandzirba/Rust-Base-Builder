26f60b5974b6f94837155f8da75d819b18cad367
krystiandzirba <krystiandzirba8820@gmail.com>
2024-01-09 17:56:20 +0100

    v1.4.0 / changed how the resources are counted, merged the structure build cost with misc build cost / misc build cost can be disabled-enabled with checkbox / reduced the resource counter size

--- src/components/script/ResourceCounter.tsx
+++ src/components/script/ResourceCounter.tsx
@@ -10,13 +10,14 @@ import hq_metalThumbnail from "../../icons/hq_metal_thumbnail.png";
 import scrapThumbnail from "../../icons/scrap_thumbnail.png";
 import gearThumbnail from "../../icons/gear_thumbnail.png";
 import sewing_kitThumbnail from "../../icons/sewing_kit_thumbnail.png";
-import lq_fuel_kitThumbnail from "../../icons/lq_fuel_thumbnail.png";
+import lq_fuelThumbnail from "../../icons/lq_fuel_thumbnail.png";
 
 export default function ResourceCounter() {
   const canvas_models_array = useSelector((state: RootState) => state.canvasModelsArray.canvas_models_array);
 
   const [build_cost, set_build_cost] = useState([{ wood: 0 }, { stone: 0 }, { metal: 0 }, { armored: 0 }]);
-  const [misc_cost, set_misc_cost] = useState([{ wood: 0}, {stone:0}, {metal: 0}, {hq_metal: 0}, {scrap:0}, {gear:0}, {sewing_kit: 0}, {lq_fuel:0}]); //prettier-ignore
+  const [components_cost, set_components_cost] = useState([{scrap:0}, {gear:0}, {sewing_kit: 0}, {lq_fuel:0}]); //prettier-ignore
+
   const [total_upkeep_percentile_rampup, set_total_upkeep_percentile_rampup] = useState(0);
   const [wood_upkeep_cost, set_wood_upkeep_cost] = useState<number>(0);
   const [stone_upkeep_cost, set_stone_upkeep_cost] = useState<number>(0);
@@ -24,9 +25,13 @@ export default function ResourceCounter() {
   const [hqm_upkeep_cost, set_hqm_upkeep_cost] = useState<number>(0);
   const [upkeep_cost_text, set_upkeep_cost_text] = useState<string>("");
 
+  const [count_miscs_cost, set_count_miscs_cost] = useState(false);
   const [total_misc_count, set_total_misc_count] = useState<number>(0);
 
-  function CountComponentsCost(models: string[]) {
+  const [total_wood_misc_cost, set_total_wood_misc_cost] = useState<number>(0);
+  const [total_metal_misc_cost, set_total_metal_misc_cost] = useState<number>(0);
+
+  function CountMiscsCost(models: string[]) {
     ////////// wood
 
     let wood_misc_cost_1000 = models.filter((model) => model === "ToolCupboard").length * 1000;
@@ -38,7 +43,7 @@ export default function ResourceCounter() {
     let wood_misc_cost_100 = models.filter((model) => model === "WoodStorageBox").length * 100;
     let wood_misc_wood_storage_box_count = models.filter((model) => model === "WoodStorageBox").length;
 
-    let total_wood_misc_cost = wood_misc_cost_1000 + wood_misc_cost_250 + wood_misc_cost_100;
+    set_total_wood_misc_cost(wood_misc_cost_1000 + wood_misc_cost_250 + wood_misc_cost_100);
 
     let total_wood_misc_count =
       wood_misc_tool_cupboard_count + wood_misc_large_wood_box_count + wood_misc_wood_storage_box_count;
@@ -48,7 +53,7 @@ export default function ResourceCounter() {
     let metal_misc_cost_50 = models.filter((model) => model === "LargeWoodBox").length * 50;
     let metal_misc_large_wood_box_count = models.filter((model) => model === "LargeWoodBox").length; //counted in the wood section already
 
-    let total_metal_misc_cost = metal_misc_cost_50;
+    set_total_metal_misc_cost(metal_misc_cost_50);
 
     let total_metal_misc_count = 0;
 
@@ -65,16 +70,7 @@ export default function ResourceCounter() {
 
     set_total_misc_count(total_wood_misc_count);
 
-    set_misc_cost([
-      { wood: total_wood_misc_cost },
-      { stone: 0 },
-      { metal: total_metal_misc_cost },
-      { hq_metal: 0 },
-      { scrap: 0 },
-      { gear: total_gear_misc_cost },
-      { sewing_kit: 0 },
-      { lq_fuel: 0 },
-    ]);
+    set_components_cost([{ scrap: 0 }, { gear: total_gear_misc_cost }, { sewing_kit: 0 }, { lq_fuel: 0 }]);
   }
 
   function CountBuildCost(models: string[]) {
@@ -286,12 +282,21 @@ models.filter(
     ////////// hq metal
     ////////// display
 
-    set_build_cost([
-      { wood: total_wood_build_cost },
-      { stone: total_stone_build_cost },
-      { metal: total_metal_build_cost },
-      { armored: total_hqm_build_cost },
-    ]);
+    if (count_miscs_cost) {
+      set_build_cost([
+        { wood: total_wood_build_cost + total_wood_misc_cost },
+        { stone: total_stone_build_cost },
+        { metal: total_metal_build_cost + total_metal_misc_cost },
+        { armored: total_hqm_build_cost },
+      ]);
+    } else if (!count_miscs_cost) {
+      set_build_cost([
+        { wood: total_wood_build_cost },
+        { stone: total_stone_build_cost },
+        { metal: total_metal_build_cost },
+        { armored: total_hqm_build_cost },
+      ]);
+    }
 
     // console.log("model count", models.length);
 
@@ -357,7 +362,9 @@ models.filter(
   }
 
   function DisplayCountedUpkeep() {
-    if (build_cost && build_cost[0] && build_cost[0].wood !== undefined) {
+    if (build_cost && build_cost[0] && build_cost[0].wood !== undefined && count_miscs_cost) {
+      set_wood_upkeep_cost((build_cost[0].wood - total_wood_misc_cost) * total_upkeep_percentile_rampup);
+    } else if (build_cost && build_cost[0] && build_cost[0].wood !== undefined && !count_miscs_cost) {
       set_wood_upkeep_cost(build_cost[0].wood * total_upkeep_percentile_rampup);
     }
 
@@ -365,7 +372,9 @@ models.filter(
       set_stone_upkeep_cost(build_cost[1].stone * total_upkeep_percentile_rampup);
     }
 
-    if (build_cost && build_cost[2] && build_cost[2].metal !== undefined) {
+    if (build_cost && build_cost[2] && build_cost[2].metal !== undefined && count_miscs_cost) {
+      set_metal_upkeep_cost((build_cost[2].metal - total_metal_misc_cost) * total_upkeep_percentile_rampup);
+    } else if (build_cost && build_cost[2] && build_cost[2].metal !== undefined && !count_miscs_cost) {
       set_metal_upkeep_cost(build_cost[2].metal * total_upkeep_percentile_rampup);
     }
 
@@ -374,11 +383,15 @@ models.filter(
     }
   }
 
+  const HandleMiscCostState = () => {
+    set_count_miscs_cost(!count_miscs_cost);
+  };
+
   useEffect(() => {
     CountBuildCost(canvas_models_array);
-    CountComponentsCost(canvas_models_array);
+    CountMiscsCost(canvas_models_array);
     CountUpkeepPercentileRampup(canvas_models_array);
-  }, [canvas_models_array, total_misc_count]);
+  }, [canvas_models_array, total_misc_count, count_miscs_cost]);
 
   useEffect(() => {
     DisplayCountedUpkeep();
@@ -386,10 +399,17 @@ models.filter(
 
   return (
     <>
+      <div className="misc_cost_checkbox">build cost</div>
+
       <div className="main_container build_cost_main_container">
-        <div className="cost_description">build cost</div>
+        <div className="misc_count_description">
+          <label>
+            <input type="checkbox" checked={count_miscs_cost} onChange={HandleMiscCostState} />
+            count miscs (TC, WB, Box, Furnace ... )
+          </label>
+        </div>
         <div className="build_cost_container">
-          <div className="build_cost_wood_container">
+          <div className="wood_container">
             <div
               className="cost_cell"
               style={{
@@ -405,7 +425,7 @@ models.filter(
               </div>
             </div>
           </div>
-          <div className="build_cost_stone_container">
+          <div className="stone_container">
             <div
               className="cost_cell"
               style={{
@@ -421,7 +441,7 @@ models.filter(
               </div>
             </div>
           </div>
-          <div className="build_cost_metal_container">
+          <div className="metal_container">
             <div className="cost_cell" style={{ backgroundImage: `url(${metalThumbnail})`, backgroundSize: "cover" }}>
               <div className="build_cost_display">
                 <div>metal f.</div>
@@ -429,13 +449,13 @@ models.filter(
               </div>
             </div>
           </div>
-          <div className="build_cost_hq_metal_container">
+          <div className="hq_metal_container">
             <div
               className="cost_cell"
               style={{ backgroundImage: `url(${hq_metalThumbnail})`, backgroundSize: "cover" }}
             >
               <div className="build_cost_display">
-                <div>hq metal</div>
+                <div>hq. metal</div>
                 <div>{build_cost[3].armored}</div>
               </div>
             </div>
@@ -448,7 +468,7 @@ models.filter(
           {upkeep_cost_text} {(total_upkeep_percentile_rampup * 100).toFixed(2)}%
         </div>
         <div className="upkeep_cost_container">
-          <div className="build_cost_wood_container">
+          <div className="wood_container">
             <div
               className="cost_cell"
               style={{
@@ -464,7 +484,7 @@ models.filter(
               </div>
             </div>
           </div>
-          <div className="build_cost_stone_container">
+          <div className="stone_container">
             <div
               className="cost_cell"
               style={{
@@ -480,7 +500,7 @@ models.filter(
               </div>
             </div>
           </div>
-          <div className="build_cost_metal_container">
+          <div className="metal_container">
             <div className="cost_cell" style={{ backgroundImage: `url(${metalThumbnail})`, backgroundSize: "cover" }}>
               <div className="build_cost_display">
                 <div>metal f.</div>
@@ -488,13 +508,13 @@ models.filter(
               </div>
             </div>
           </div>
-          <div className="build_cost_hq_metal_container">
+          <div className="hq_metal_container">
             <div
               className="cost_cell"
               style={{ backgroundImage: `url(${hq_metalThumbnail})`, backgroundSize: "cover" }}
             >
               <div className="build_cost_display">
-                <div>hq metal</div>
+                <div>hq. metal</div>
                 <div>{hqm_upkeep_cost.toFixed(0)}</div>
               </div>
             </div>
@@ -502,66 +522,12 @@ models.filter(
         </div>
       </div>
 
-      <div className="main_container misc_cost_main_container_a">
-        <div className="cost_description">misc cost (T.CB, WB, Box, Furnace ... )</div>
-        <div className="upkeep_cost_container">
-          <div className="build_cost_wood_container">
-            <div
-              className="cost_cell"
-              style={{
-                backgroundImage: `url(${woodThumbnail})`,
-                backgroundSize: "contain",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center center",
-              }}
-            >
-              <div className="build_cost_display">
-                <div>wood</div>
-                <div>{misc_cost[0].wood}</div>
-              </div>
-            </div>
-          </div>
-          <div className="build_cost_stone_container">
-            <div
-              className="cost_cell"
-              style={{
-                backgroundImage: `url(${stoneThumbnail})`,
-                backgroundSize: "contain",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center center",
-              }}
-            >
-              <div className="build_cost_display">
-                <div>stone</div>
-                <div>{misc_cost[1].stone}</div>
-              </div>
-            </div>
-          </div>
-          <div className="build_cost_metal_container">
-            <div className="cost_cell" style={{ backgroundImage: `url(${metalThumbnail})`, backgroundSize: "cover" }}>
-              <div className="build_cost_display">
-                <div>metal f.</div>
-                <div>{misc_cost[2].metal}</div>
-              </div>
-            </div>
-          </div>
-          <div className="build_cost_hq_metal_container">
-            <div
-              className="cost_cell"
-              style={{ backgroundImage: `url(${hq_metalThumbnail})`, backgroundSize: "cover" }}
-            >
-              <div className="build_cost_display">
-                <div>hq metal</div>
-                <div>{misc_cost[3].hq_metal}</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      {/* aaaaa */}
 
-      <div className="main_container_b misc_cost_main_container_b">
+      <div className="main_container components_cost_main_container">
+        <div className="cost_description">components cost</div>
         <div className="upkeep_cost_container">
-          <div className="misc_const_container_b">
+          <div className="scrap_container">
             <div
               className="cost_cell"
               style={{
@@ -571,13 +537,13 @@ models.filter(
                 backgroundPosition: "center center",
               }}
             >
-              <div className="build_cost_display_b">
+              <div className="build_cost_display">
                 <div>scrap</div>
-                <div>{misc_cost[4].scrap}</div>
+                <div>{components_cost[0].scrap}</div>
               </div>
             </div>
           </div>
-          <div className="misc_const_container_b">
+          <div className="gears_container">
             <div
               className="cost_cell"
               style={{
@@ -587,31 +553,28 @@ models.filter(
                 backgroundPosition: "center center",
               }}
             >
-              <div className="build_cost_display_b">
-                <div>gear</div>
-                <div>{misc_cost[5].gear}</div>
+              <div className="build_cost_display">
+                <div>gears</div>
+                <div>{components_cost[1].gear}</div>
               </div>
             </div>
           </div>
-          <div className="misc_const_container_b">
+          <div className="sewing_kit_container">
             <div
               className="cost_cell"
               style={{ backgroundImage: `url(${sewing_kitThumbnail})`, backgroundSize: "cover" }}
             >
-              <div className="build_cost_display_b">
+              <div className="build_cost_display">
                 <div>sewing kit</div>
-                <div>{misc_cost[6].sewing_kit}</div>
+                <div>{components_cost[2].sewing_kit}</div>
               </div>
             </div>
           </div>
-          <div className="misc_const_container_b">
-            <div
-              className="cost_cell"
-              style={{ backgroundImage: `url(${lq_fuel_kitThumbnail})`, backgroundSize: "cover" }}
-            >
-              <div className="build_cost_display_b">
+          <div className="lq_fuel_container">
+            <div className="cost_cell" style={{ backgroundImage: `url(${lq_fuelThumbnail})`, backgroundSize: "cover" }}>
+              <div className="build_cost_display">
                 <div>lq. fuel</div>
-                <div>{misc_cost[7].lq_fuel}</div>
+                <div>{components_cost[3].lq_fuel}</div>
               </div>
             </div>
           </div>
