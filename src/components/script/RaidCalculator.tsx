24814a442bf58715dcb7f47b97bf33d6894b48bd
krystiandzirba <krystiandzirba8820@gmail.com>
2024-01-12 01:25:22 +0100

    v1.10.0 / added a function to calculate rocket raid cost

--- src/components/script/RaidCalculator.tsx
+++ src/components/script/RaidCalculator.tsx
@@ -2,15 +2,82 @@ import { useDispatch } from "react-redux";
 import { RootState } from "../../Store.tsx";
 import { useSelector } from "react-redux";
 import { set_reset_raid_models } from "../../Store.tsx";
+import { useEffect, useState } from "react";
 
 export default function RaidCalculator() {
   const dispatch = useDispatch();
 
+  const page_mode = useSelector((state: RootState) => state.pageMode.page_mode);
   const reset_raid_models = useSelector((state: RootState) => state.modelsData.reset_raid_models); //prettier-ignore
+  const model_destroy_tigger = useSelector((state: RootState) => state.modelsData.model_destroy_trigger); //prettier-ignore
+  const model_to_destroy = useSelector((state: RootState) => state.modelsData.model_to_destroy); //prettier-ignore
+
+  const [raid_tool, set_raid_tool] = useState<string>("efficiency");
+  const [rocket_cost, set_rocket_cost] = useState<number>(0);
 
   function ResetRaid() {
     dispatch(set_reset_raid_models(!reset_raid_models));
+
+    set_rocket_cost(0);
   }
+
+  function CalculateRocketCost() {
+    if (
+      model_to_destroy === "StoneFoundationSquareHigh" ||
+      model_to_destroy === "StoneFoundationSquareMid" ||
+      model_to_destroy === "StoneFoundationSquareLow" ||
+      model_to_destroy === "StoneFoundationTriangleHigh" ||
+      model_to_destroy === "StoneFoundationTriangleMid" ||
+      model_to_destroy === "StoneFoundationTriangleLow" ||
+      model_to_destroy === "StoneWallHigh" ||
+      model_to_destroy === "StoneWallMid" ||
+      model_to_destroy === "StoneWallLow" ||
+      model_to_destroy === "StoneDoorway" ||
+      model_to_destroy === "StoneWindow" ||
+      model_to_destroy === "StoneStairsLShape" ||
+      model_to_destroy === "StoneStairsUShape" ||
+      model_to_destroy === "StoneWallFrame" ||
+      model_to_destroy === "StoneFloorSquare" ||
+      model_to_destroy === "StoneFloorTriangle" ||
+      model_to_destroy === "StoneFloorFrameSquare" ||
+      model_to_destroy === "StoneFloorFrameTriangle"
+    ) {
+      set_rocket_cost(rocket_cost + 2);
+    } else if (
+      model_to_destroy === "MetalFoundationSquareHigh" ||
+      model_to_destroy === "MetalFoundationSquareMid" ||
+      model_to_destroy === "MetalFoundationSquareLow" ||
+      model_to_destroy === "MetalFoundationTriangleHigh" ||
+      model_to_destroy === "MetalFoundationTriangleMid" ||
+      model_to_destroy === "MetalFoundationTriangleLow" ||
+      model_to_destroy === "MetalWallHigh" ||
+      model_to_destroy === "MetalWallMid" ||
+      model_to_destroy === "MetalWallLow" ||
+      model_to_destroy === "MetalDoorway" ||
+      model_to_destroy === "MetalWindow" ||
+      model_to_destroy === "MetalStairsLShape" ||
+      model_to_destroy === "MetalStairsUShape" ||
+      model_to_destroy === "MetalWallFrame" ||
+      model_to_destroy === "MetalFloorSquare" ||
+      model_to_destroy === "MetalFloorTriangle" ||
+      model_to_destroy === "MetalFloorFrameSquare" ||
+      model_to_destroy === "MetalFloorFrameTriangle"
+    ) {
+      set_rocket_cost(rocket_cost + 4);
+    }
+  }
+
+  useEffect(() => {
+    {
+      CalculateRocketCost();
+    }
+  }, [model_destroy_tigger]);
+
+  useEffect(() => {
+    {
+      set_rocket_cost(0);
+    }
+  }, [page_mode]);
 
   return (
     <>
@@ -27,7 +94,7 @@ export default function RaidCalculator() {
       </div>
 
       <div className="main_container raid_tools_cost_main_container">
-        <div className="cost_description">raid cost (tools)</div>
+        <div className="cost_description">raid cost - hard side (type)</div>
         <div className="raid_cost_container">
           <div className="rocket_container">
             <div
@@ -41,7 +108,7 @@ export default function RaidCalculator() {
             >
               <div className="raid_cost_display">
                 <div>rocket</div>
-                <div>0</div>
+                <div>{rocket_cost}</div>
               </div>
             </div>
           </div>
